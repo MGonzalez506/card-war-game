d56845397b174bc6075056f258dbc49626e3aba7
mgonzalez506 <MGonzalez506@users.noreply.github.com>
2023-03-21 18:37:12 -0600

    Código incial

---
+++ card-war-game/card_war_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  card_war_gameApp.swift
+//  card-war-game
+//
+//  Created by MIGUEL GONZALEZ on 18/11/22.
+//
+
+import SwiftUI
+
+@main
+struct card_war_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
